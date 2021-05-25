a3ea062d624bdbfccfb2c17ea51e10b5d151c9e7
okamoto-tosei <tosei393939@gmail.com>
2021-05-25 23:37:59 +0900

    gridCSSの追加

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -9,13 +9,14 @@ const SongList = (props) => {
           <div className="right floated content">
             <button className="ui button primary">Select</button>
           </div>
+          <div className="content">{song.title}</div>
         </div>
       );
     });
   };
   return (
     <div>
-      <p>SongList</p>
+      <div className="ui divided list">{renderList()}</div>
     </div>
   );
 };
